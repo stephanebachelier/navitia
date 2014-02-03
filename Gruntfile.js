901f48d7938f1bde849642f2dba24b6dcf8c6efe
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-02-03 23:16:06 +0100

    fix missing grunt task dep

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,6 +68,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-release');
 
   // Default task.
   grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
