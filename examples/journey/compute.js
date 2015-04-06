c3ec82615236b9bdc9234e0cd809d9860ff6098b
Stéphane Bachelier <stephane.bachelier@gmail.com>
2015-04-06 12:53:16 +0200

    feat(example): update journey examples

--- examples/journey/compute.js
+++ examples/journey/compute.js
@@ -1,14 +1,19 @@
-var n = require(process.cwd() + '/dist/navitia');
+var n = require('../../lib/navitia');
+require('dotenv').load()
+var moment = require('moment');
+var descriptor = require('./descriptor');
+
+var date = moment(Date.now()).format('YYYYMMDDTHHmm');
 
 // use /v1/journey
-n.query()
+n.query({apikey: process.env.apikey, debug: process.env.debug})
   .journey(null, {
     from: '2.2627899;48.984186',
     to: '2.363871;48.8673191',
-    datetime: '20140204T0800'
-    //max_duration: 1200
+    datetime: date
   })
   .end()
   .on('ready', function (res) {
     console.log('found #' + res.resource.length, ' journeys.');
+    res.resource.forEach(descriptor);
   });
