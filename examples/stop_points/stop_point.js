6ded1b58648a1fdfddcf57a17069709f5bd7a008
Stéphane Bachelier <stephane.bachelier@gmail.com>
2015-04-06 17:16:41 +0200

    feat(stop_points): update example

--- examples/stop_points/stop_point.js
+++ examples/stop_points/stop_point.js
@@ -1,13 +1,15 @@
-var n = require(process.cwd() + '/dist/navitia');
+var n = require('../../lib/navitia');
+require('dotenv').load()
 
-n.query()
-  .region('paris')
+n.query({apikey: process.env.apikey, debug: process.env.debug })
+  .region('fr-idf')
   .stoppoints('stop_point:TRN:SP:DUA8711611')
   .end()
   .on('ready', function (res) {
     console.log('found #' + res.resource.length, ' stop points.');
     var sp = res.resource[0];
-    console.log(' * name', sp.name);
-    console.log(' * coord', sp.coord.lat, sp.coord.lon);
-
+    console.log(' * id:', sp.id);
+    console.log(' * name:', sp.name);
+    console.log(' * address:', sp.address.label);
+    console.log(' * coord:', sp.coord.lat, sp.coord.lon);
   });
