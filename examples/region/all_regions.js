905f2539a98e4ae793247e8b7d6312b4d83be38c
Stéphane Bachelier <stephane.bachelier@gmail.com>
2015-04-06 15:12:31 +0200

    feat(region): update example

--- examples/region/all_regions.js
+++ examples/region/all_regions.js
@@ -1,5 +1,19 @@
-var n = require(process.cwd() + '/dist/navitia');
+var n = require('../../lib/navitia');
+require('dotenv').load()
 
-n.query().regions().end().on('ready', function (res) {
-  console.log('ready', res.resource);
-});
+n.query({apikey: process.env.apikey, debug: process.env.debug })
+  .regions()
+  .end()
+  .on('ready', function (res) {
+    if (!res.resource) {
+      console.log('No regions found');
+      return;
+    }
+    console.log('Found #' + res.resource.length, 'regions.');
+    res.resource.forEach(function (region) {
+      console.log(' --------------------------- ');
+      console.log(' * id:', region.id);
+      console.log(' * start_production_date:', region.start_production_date);
+      console.log(' * end_production_date:', region.end_production_date);
+    })
+  });
