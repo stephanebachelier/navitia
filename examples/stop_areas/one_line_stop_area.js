84d68e499c73844dafea0b3846a22c63ebbcdc0c
Stéphane Bachelier <stephane.bachelier@gmail.com>
2015-04-06 15:29:16 +0200

    feat(stoparea): update example

--- examples/stop_areas/one_line_stop_area.js
+++ examples/stop_areas/one_line_stop_area.js
@@ -1,12 +1,19 @@
-var n = require(process.cwd() + '/dist/navitia');
+var n = require('../../lib/navitia');
+require('dotenv').load()
 
-n.query()
-  .region('paris')
+n.query({apikey: process.env.apikey, debug: process.env.debug })
+  .region('fr-idf')
   .network('network:TN')
-  .lines('line:TRN:DUA800850013')
+  .lines('line:TRN:DUA800850102')
   .stopareas()
   .end()
   .on('ready', function (res) {
     console.log('found #' + res.resource.length, ' stop areas.');
-    console.log(res.resource.map(function (stoparea) { return stoparea.name; }));
+    res.resource.map(function (stoparea) {
+      console.log(' --------------------------------------');
+      console.log(' * id:', stoparea.id);
+      console.log(' * label:', stoparea.label);
+      console.log(' * lat:', stoparea.coord.lat);
+      console.log(' * lon:', stoparea.coord.lon);
+    });
   });
